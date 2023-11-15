eb60089b2976fb7ce11edad0171af13343b1c834
Antoine Zen-Ruffinen <antoine.zen@gmail.com>
2023-11-15 18:01:55 +0100

    [MODIFY] Change for the `App` object to hold connection to DB instead of DB file name.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,42 @@
 use anyhow::{anyhow, Context, Result};
 use chrono::{DateTime, Duration, Utc};
-use clap::{command, Command};
+use clap::{command, Arg, Command};
 use now::DateTimeNow;
-use sqlite::Connection;
 use wtime::db::InOut::{In, Out};
 use wtime::db::{InOut, Stamp};
 
-
 /// Datacontainer for application live variables
 struct App {
-    /// Database name
-    work_db: String,
+    /// Database connection
+    conn: sqlite::Connection,
 }
 
-
 impl App {
+    fn new(db_name: String) -> Result<Self> {
+        let db_file = std::path::Path::new(&db_name);
+        let must_init = !db_file.exists();
+        let conn = sqlite::open(db_file)?;
 
-    fn new(db_name: String) -> Self{
-        Self{work_db: db_name}
+        if must_init {
+            Stamp::create(&conn).context("Crate Stamp table")?;
+        }
+        Ok(Self { conn })
     }
 
-    /// Get total worked time since given date `from` using database connection `c`.
-    fn get_total_from(self: &Self, c: &Connection, from: &DateTime<Utc>) -> Duration {
+    /// Get total worked time since given date `from`.
+    fn get_total_from(self: &Self, from: &DateTime<Utc>) -> Duration {
         let mut total = Duration::zero();
         let mut possible_last: Option<Stamp> = None;
 
         // Get first stamp after given date, it there is none, return Zero duration
-        let first = if let Ok(s) = Stamp::get_after(c, from) {
+        let first = if let Ok(s) = Stamp::get_after(&self.conn, from) {
             s
         } else {
             return Duration::zero();
         };
 
         // Iterate on all stamps from there and sum the total
-        for stamp in first.iter(c) {
+        for stamp in first.iter(&self.conn) {
             if let Some(l) = possible_last {
                 if l.in_out == In && stamp.in_out == Out {
                     total = total + (stamp.date - l.date);
@@ -46,12 +49,12 @@ impl App {
         total
     }
 
-    fn print_resume(self: &Self, c: &Connection) {
+    fn print_resume(self: &Self) {
         // Print worked time
         let now = Utc::now();
 
         let begin_of_day = now.beginning_of_day();
-        let day_total = self.get_total_from(c, &begin_of_day);
+        let day_total = self.get_total_from(&begin_of_day);
         println!(
             "You worked {} hours, {} minutes and {} seconds today (since {})",
             day_total.num_hours(),
@@ -63,7 +66,7 @@ impl App {
         // Don't show week total on mondays
         let begin_of_week = now.beginning_of_week();
         if begin_of_day != begin_of_week {
-            let week_total = self.get_total_from(c, &begin_of_week);
+            let week_total = self.get_total_from(&begin_of_week);
             println!(
                 "You worked {} hours, {} minutes and {} seconds this week (since {})",
                 week_total.num_hours(),
@@ -74,23 +77,9 @@ impl App {
         }
     }
 
-    /// Open database connection, creating the table if the database does not previously exits.
-    fn open_db(self: &Self) -> Result<sqlite::Connection> {
-        let db_file = std::path::Path::new(&self.work_db);
-        let must_init = !db_file.exists();
-        let conn = sqlite::open(db_file)?;
-
-        if must_init {
-            Stamp::create(&conn).context("Crate Stamp table")?;
-        }
-        Ok(conn)
-    }
-
     fn do_checkin(self: &Self) -> Result<()> {
-        let c = self.open_db().context("Opening database")?;
-
         // check that we are actually out
-        if let Some(last_stamp) = Stamp::last(&c) {
+        if let Some(last_stamp) = Stamp::last(&self.conn) {
             if last_stamp.in_out == InOut::In {
                 return Err(anyhow!(
                     "Already checked in ! (Do you meant to check-out ?)"
@@ -100,17 +89,15 @@ impl App {
 
         // Creat teh checking stamp
         let mut stamp = Stamp::check_in();
-        stamp.insert(&c).context("Inserting new stamp")?;
+        stamp.insert(&self.conn).context("Inserting new stamp")?;
 
         println!("Checked in at {}", stamp.date.format("%H:%M"));
         Ok(())
     }
 
     fn do_checkout(self: &Self) -> Result<()> {
-        let c = self.open_db().context("Opening database")?;
-
         // Check that last stamp is check-in
-        if let Some(last_stamp) = Stamp::last(&c) {
+        if let Some(last_stamp) = Stamp::last(&self.conn) {
             if last_stamp.in_out == InOut::Out {
                 return Err(anyhow!(
                     "Already checked out ! (Do you meant to check-in ?)"
@@ -120,11 +107,11 @@ impl App {
 
         // Create the checkout stamps
         let mut stamp = Stamp::check_out();
-        stamp.insert(&c).context("Inserting new stamp")?;
+        stamp.insert(&self.conn).context("Inserting new stamp")?;
 
         println!("Checked out at {}", stamp.date.format("%H:%M"));
 
-        if let Some(checkin) = stamp.previous(&c) {
+        if let Some(checkin) = stamp.previous(&self.conn) {
             let work_time = checkin.delta(&stamp);
             println!(
                 "You worked {} hours, {} minutes and {} seconds",
@@ -138,20 +125,28 @@ impl App {
     }
 
     fn do_list(self: &Self) -> Result<()> {
-        let c = self.open_db().context("Opening database")?;
-        self.print_resume(&c);
+        self.print_resume();
         Ok(())
     }
 }
 
-
 fn main() -> Result<()> {
     let matches = command!()
+        .arg(
+            Arg::new("database")
+                .long("db")
+                .default_value("prod.sqlite")
+                .help("Specify database to use"),
+        )
         .subcommand(Command::new("checkin").about("Start counting working time"))
         .subcommand(Command::new("checkout").about("Stop counting work time and display count"))
         .get_matches();
 
-    let app = App::new("prod.sqlite".to_string());
+    let db_file_name: String = matches
+        .get_one::<String>("database")
+        .map(|s| s.to_string())
+        .context("Get DB file")?;
+    let app = App::new(db_file_name).context("Open DB file")?;
 
     match matches.subcommand() {
         Some(("checkin", _)) => app.do_checkin(),
