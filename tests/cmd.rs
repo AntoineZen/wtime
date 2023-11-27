e6690e496730468ec117dae0d43d9adaf411088c
Antoine Zen-Ruffinen <antoine.zen@gmail.com>
2023-11-27 22:34:57 +0100

    [TEST] Add more test for checkin/checkout.

--- tests/cmd.rs
+++ tests/cmd.rs
@@ -1,11 +1,102 @@
 use assert_cmd::*;
+use std::fs;
 
+const TEST_FILE: &str = "test.sqlite";
+fn teardown() {
+    fs::remove_file(TEST_FILE).unwrap();
+}
 #[test]
 fn test_default() {
     Command::cargo_bin(env!("CARGO_PKG_NAME"))
         .unwrap()
         .arg("--db")
-        .arg("test.sqlite")
+        .arg(TEST_FILE)
         .assert()
         .success();
+
+    teardown();
+}
+
+#[test]
+fn test_checkin_checkout() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkin")
+        .assert()
+        .success();
+
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkout")
+        .assert()
+        .success();
+
+    teardown();
+}
+
+#[test]
+fn test_double_checkin() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkin")
+        .assert()
+        .success();
+
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkin")
+        .assert()
+        .failure();
+
+    teardown();
+}
+
+#[test]
+fn test_first_checkout() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkout")
+        .assert()
+        .success();
+
+    teardown();
+}
+
+#[test]
+fn test_double_checkout() {
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkin")
+        .assert()
+        .success();
+
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkout")
+        .assert()
+        .success();
+
+    Command::cargo_bin(env!("CARGO_PKG_NAME"))
+        .unwrap()
+        .arg("--db")
+        .arg(TEST_FILE)
+        .arg("checkout")
+        .assert()
+        .failure();
+
+    teardown();
 }
